242f6d8f09a98e975bc9749d5a088edf0f2e025c
Hiroaki Yutani <yutani.ini@gmail.com>
2022-09-07 09:16:35 +0900

    Update

--- wgsl-cli/src/main.rs
+++ wgsl-cli/src/main.rs
@@ -1,3 +1,5 @@
+use wgpu::{include_wgsl, util::DeviceExt};
+
 use winit::{
     dpi::LogicalSize,
     event::*,
@@ -9,12 +11,43 @@ use winit::{
 const HEIGHT: f64 = 1000.0;
 const WIDTH: f64 = 1000.0;
 
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 2],
+}
+
+impl Vertex {
+    const ATTRIBS: [wgpu::VertexAttribute; 1] = wgpu::vertex_attr_array![0 => Float32x2];
+
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Self>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBS,
+        }
+    }
+}
+
+#[rustfmt::skip]
+const RECT_VERTICES: &[Vertex] = &[
+    Vertex { position: [ 1.0, -1.0] },
+    Vertex { position: [-1.0, -1.0] },
+    Vertex { position: [-1.0,  1.0] },
+    Vertex { position: [ 1.0,  1.0] },
+];
+const RECT_INDICES: &[u16] = &[0, 1, 2, 0, 2, 3];
+
 struct State {
     surface: wgpu::Surface,
     device: wgpu::Device,
     queue: wgpu::Queue,
+
     config: wgpu::SurfaceConfiguration,
+
     size: winit::dpi::PhysicalSize<u32>,
+
+    render_pipeline: wgpu::RenderPipeline,
 }
 
 impl State {
@@ -54,12 +87,59 @@ impl State {
 
         surface.configure(&device, &config);
 
+        let vertex_shader = device.create_shader_module(include_wgsl!("vert.wgsl"));
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &vertex_shader,
+                entry_point: "vs_main",
+                buffers: &[],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &vertex_shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: None, // TODO: what's the correct value here?
+                unclipped_depth: false,
+                polygon_mode: wgpu::PolygonMode::Fill,
+                conservative: false,
+            },
+            // we don't use depth
+            depth_stencil: None,
+            // we don't use multisample
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                ..Default::default()
+            },
+            // we don't use aray textures
+            multiview: None,
+        });
+
         Self {
             surface,
             device,
             queue,
             config,
             size,
+            render_pipeline,
         }
     }
 
@@ -87,7 +167,7 @@ impl State {
             });
 
         {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass Descriptor"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                     view: &view,
@@ -99,6 +179,9 @@ impl State {
                 })],
                 depth_stencil_attachment: None,
             });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
         }
 
         self.queue.submit(std::iter::once(encoder.finish()));
